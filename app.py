6431de28c83630be1346558e85338c2fdc6d65dd
Fl3xi99 <tvu1@telenet.be>
2025-06-05 10:35:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -89,5 +89,7 @@ def logout():
     session.clear()
     return redirect('/')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=True)
